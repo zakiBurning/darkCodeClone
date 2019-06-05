d122c4c078f82969cd1d8b1d11aa72eafc71b0cd
zakiBurning <devastator.kings@gmail.com>
2019-06-05 13:33:38 +0900

    Detail... TLDR: Implementation to client-server architecture, loading index.html successfully #client-server architecture  +port: 8081  +Using with express nodemon
    #Loading index.html successfully
     +Display "Loading..." as span element
     +not apply style.css. Probably, due to MIME type, text/html and so on

--- LoadAnimationWithUsingOnlyHTML_CSS/server.js
+++ LoadAnimationWithUsingOnlyHTML_CSS/server.js
@@ -1,9 +1,21 @@
-let app = require('express')();
+let express = require('express');
+
+let app = express();
+
+app.use(express.static('public'));
+
+
 
 
 app.get('/', function (req, res) {
     res.send("Hello World");
 })
+
+app.get('/index.html', function (req, res) {
+    console.log("GET:Dark Code Loaded")
+    res.sendFile(__dirname + "/public/" + "index.html");
+})
+
 
 let server = app.listen(8081, function () {
     let host = server.address().address;
